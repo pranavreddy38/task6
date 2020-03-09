cb826ea1416d522819e0b091aedc478c3a28f993
pranavreddy38 <pranavreddy38@gmail.com>
2020-03-09 09:52:30 +0530

    task6

---
+++ App/src/main/java/eapm/App/App.java
@@ -0,0 +1,13 @@
+package eapm.App;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
